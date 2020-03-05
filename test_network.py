b64d997d11d55db77b0bd9c34e5fb0d404997a88
yunsmre <emrekoc@tutamail.com>
2020-03-05 14:30:01 +0100

    Edittest

--- test_network.py
+++ test_network.py
@@ -14,14 +14,14 @@ message_ = {
 json_packet = json.dumps(message_)
 json_packet = bytes(json_packet, "ascii")
 network = Network.Network("localhost", 20009)
-p1 = Thread(target = network.UDP_listen, args=(20009,))
+#p1 = Thread(target = network.UDP_listen, args=(20009,))
 p2 = Thread(target =network.UDP_broadcast, args=(json_packet, '<broadcast>', 20008,))
-p1.start()
+#p1.start()
 p2.start()
 while True:
     p2.join(timeout=3)
     try:
-        p1.join(timeout=3)
+        #p1.join(timeout=3)
     except:
         pass
     #network.UDP_broadcast(json_packet, '<broadcast>', 20009)
